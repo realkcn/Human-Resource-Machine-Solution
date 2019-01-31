20ba0a774b9beca0eec2d89f0ec52bc572b56cd6
KCN <realkcn@gmail.com>
2019-02-01 01:00:17 +0800

    Create 38-speed.asm

---
+++ 38-speed.asm
@@ -0,0 +1,194 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+
+a:
+    COPYFROM 9
+    COPYTO   3
+    COPYTO   4
+    INBOX   
+    SUB      11
+    JUMPN    i
+    SUB      11
+    JUMPN    q
+    SUB      11
+    JUMPN    p
+    SUB      11
+    JUMPN    o
+    SUB      11
+    JUMPN    n
+    SUB      11
+    JUMPN    m
+    SUB      11
+    JUMPN    l
+    SUB      11
+    JUMPN    k
+    SUB      11
+    JUMPN    j
+    COPYTO   0
+    BUMPUP   3
+b:
+    BUMPUP   3
+c:
+    BUMPUP   3
+d:
+    BUMPUP   3
+e:
+    BUMPUP   3
+f:
+    BUMPUP   3
+g:
+    BUMPUP   3
+h:
+    BUMPUP   3
+    BUMPUP   3
+    JUMP     r
+i:
+    ADD      11
+    JUMP     s
+j:
+    ADD      11
+    COPYTO   0
+    JUMP     b
+k:
+    ADD      11
+    COPYTO   0
+    JUMP     c
+l:
+    ADD      11
+    COPYTO   0
+    JUMP     d
+m:
+    ADD      11
+    COPYTO   0
+    JUMP     e
+n:
+    ADD      11
+    COPYTO   0
+    JUMP     f
+o:
+    ADD      11
+    COPYTO   0
+    JUMP     g
+p:
+    ADD      11
+    COPYTO   0
+    JUMP     h
+q:
+    ADD      11
+    COPYTO   0
+    BUMPUP   3
+r:
+    OUTBOX  
+    COPYFROM 0
+s:
+    SUB      10
+    JUMPN    ai
+    SUB      10
+    JUMPN    ah
+    SUB      10
+    JUMPN    ag
+    SUB      10
+    JUMPN    af
+    SUB      10
+    JUMPN    ae
+    SUB      10
+    JUMPN    ad
+    SUB      10
+    JUMPN    ac
+    SUB      10
+    JUMPN    ab
+    SUB      10
+    JUMPN    aa
+    COPYTO   0
+    BUMPUP   4
+t:
+    BUMPUP   4
+u:
+    BUMPUP   4
+v:
+    BUMPUP   4
+w:
+    BUMPUP   4
+x:
+    BUMPUP   4
+y:
+    BUMPUP   4
+z:
+    BUMPUP   4
+    BUMPUP   4
+    JUMP     aj
+aa:
+    ADD      10
+    COPYTO   0
+    JUMP     t
+ab:
+    ADD      10
+    COPYTO   0
+    JUMP     u
+ac:
+    ADD      10
+    COPYTO   0
+    JUMP     v
+ad:
+    ADD      10
+    COPYTO   0
+    JUMP     w
+ae:
+    ADD      10
+    COPYTO   0
+    JUMP     x
+af:
+    ADD      10
+    COPYTO   0
+    JUMP     y
+ag:
+    ADD      10
+    COPYTO   0
+    JUMP     z
+ah:
+    ADD      10
+    COPYTO   0
+    BUMPUP   4
+    JUMP     ak
+ai:
+    ADD      10
+    COPYTO   0
+    COPYFROM 4
+    ADD      3
+    JUMPZ    al
+    SUB      3
+aj:
+ak:
+    OUTBOX  
+al:
+    COPYFROM 0
+    OUTBOX  
+    JUMP     a
+
+
+DEFINE LABEL 0
+eJzTZmBg6NAP8RPS2TlNSnP2+jCtrsXdepndX8x2Zjy3nh+WYBcRGO0wPyzCcUvqXPtrtWo2f2assHZf
+mGZzbilQK4Ot95p4B697G9OD3RfGhJtN5Y09V+4VV5fDGzu78VzMzmkc8ZdWHIu/tvpYvOAKkPpl2d/T
+l2XHNK3IEl29LLtprnjh5Gq38g1Je6vUos1qvcu+1C5pN667tKKgvm3B86Ytk3VnPIpIn8VRkjjnzwyG
+UTAKRgHVAQC7lUvk;
+
+DEFINE LABEL 3
+eJyTZGBg2GTu7bHD9KSbi+FZTyGdiMB56itiUlTN0lcqnyvPUWKYs0zl1jp59SVbb2mobWcxTN32xlJy
+HVAbQ6vHZN/8gMzY1JCqzPiwc+XzwtQ6lUN3TnsX0LVYzClk1W/vgOXKoddWM4yCUTAKBi0AAPxpJ2M;
+
+DEFINE LABEL 4
+eJwTYWBgWBl0zX9NoLdHqV+Xe7+LbGiazYakHaYshSyGtxp89D5POqvLMt9HL2RVh/69jf+NMneqhjCc
+SgplOCUbvvMIUDtDYu6toNn5/RVhhRwXGEbBKBgFQwoAALm3H08;
+
+DEFINE LABEL 9
+eJwTZmBgiNTcYRWpOT8sR6l/2Wql6WvUVRI3z1NfslVE+/GW76Yhq+TsGeZEOP6YOtUpt2+qE0/5Hftb
+Qcm2TY4Z1maWepaFpv+NXhuEaWXqM4yCUTAKhhwAAB+UHcc;
+
+DEFINE LABEL 10
+eJyTYGBgSLOxz11qI1qrY+U875N54ua9xicvAIUZtIP6KwwCNkzID7i04nXg/E2qIa93zQszOiAUXXao
+JaFwv0nakq1aWbxL1bONJq3OFK0tTG/K352sl7gv6XVcY+LruJdBf7IYRsEoGAWDGgAAceApjg;
+
+DEFINE LABEL 11
+eJxTZWBgWMPl7ZHFHRH4hqs0rYpVch1QiCFSszTtseqthhTVwomPVX9MlVffO3O2hv1sft26WXuNt0x+
+ZaXWGWsvWHXHvin/kW1uwiqrmJBN5tf8t5rNDysze58IMoMrrr/iQvT0ujnhGybIhu+cdjt870yWimu1
+P6s5Sixr9mY3VGrFdZTFhAQWT/adUSDqvzktN8E/5mAewygYBaOAbgAAdmE8fw;
+
