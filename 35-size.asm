a9ccea8e73f691ca7b371ba7285a1a0e1c7b4ad5
KCN <realkcn@gmail.com>
2019-02-01 00:17:09 +0800

    Create 35-size.asm

---
+++ 35-size.asm
@@ -0,0 +1,40 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+
+a:
+b:
+    INBOX   
+    COPYTO   11
+    COPYFROM 14
+    COPYTO   12
+    JUMPZ    e
+c:
+    BUMPDN   12
+    JUMPN    d
+    COPYFROM [12]
+    SUB      11
+    JUMPZ    b
+    JUMP     c
+d:
+e:
+    COPYFROM 11
+    COPYTO   [14]
+    OUTBOX  
+    BUMPUP   14
+    JUMP     a
+
+
+DEFINE LABEL 11
+eJwTYmBgcEmYXN2ScK78WLx78Ym4g3k9sQx5NyJ4yvMDcvsYvfqXHXd/vIXN4/1eZ88fh619GE6phnRf
+ZEoMuPoqs/tiSk7T6cU5dScYRsEoGAVDEgAA+pUhGw;
+
+DEFINE LABEL 12
+eJzjZmBgMPbfO1M9W7BqYnTbgjnhshtvhz/fLhr1epd56vPtQGkG+Twez7l5gj4Mo2AUjIJhBwDTohA8
+;
+
+DEFINE LABEL 14
+eJyzYmBgUFWVDf0srxXnJc5RMkX4XnO4kPUMAdGmueVyHIueqrQtkNK0nw1UxpBg9z1dx+parY7V47ZX
+Vs87n9g874y1T2i97ihaK+x8siTAxShF0CUzVsxpfpik471QkJ7wyOfRMeHv9/5NOrkoLm9Nr1rz+8Sf
+1c87q6t3TvtZvXfm95q9M9c2LGkHqfXqrcvx7ZnfMqWzaa5w14+p3L23Ghwm2ufWTNqZUTX5e3rJVIa8
+d9MvVaXPsp/9ePbemeELCic6rhCtfb9uZ8bcrSF+Ijv/2JzZLas+a1tb0Z2tVdNBZhqu4/HkvsLjyTAK
+RsEowAoAFM9lng;
+
