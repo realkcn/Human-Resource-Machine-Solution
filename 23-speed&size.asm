8b172c8ade2fd46ba99dcdc67535c65ab8c43ffd
KCN <realkcn@gmail.com>
2019-02-01 00:05:42 +0800

    Create 23-speed&size.asm

---
+++ 23-speed&size.asm
@@ -0,0 +1,31 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+
+a:
+    INBOX   
+    COPYTO   0
+b:
+c:
+    INBOX   
+    JUMPZ    e
+    SUB      0
+    JUMPN    d
+    JUMP     c
+d:
+    ADD      0
+    COPYTO   0
+    JUMP     b
+e:
+    COPYFROM 0
+    OUTBOX  
+    JUMP     a
+
+
+DEFINE LABEL 0
+eJyTZ2BgEBbhKZ8oElEfJHJpRaPUii4pzUcR07RYCoV0BFcUm6/oEnZuKwpwiWkKcLm2GqicQTm0Kf9e
+2JL22+EnF4H4CmGX5OaEy6qD2DmZRikrsnL7FHP7K8QLcxN6S6YHuJWf9WyoPOm2t6rL/UvtraD39XqJ
+i1rqZjGMglEwCgYcAAC6mC5U;
+
+DEFINE LABEL 1
+eJwTZGBgiNR83SOirde/wWLL5NzAqukHEqumA4UZ0oPfJ372s57x00dvb7kv7+WN/jH30oPVnl6LXPOy
+Jjn3VWLu6xfXC1Y8YxgFo2AUDFkAAB2EH5g;
+
